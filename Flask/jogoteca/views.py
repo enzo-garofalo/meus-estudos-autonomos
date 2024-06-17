ac4cdc521581d856fa210b6b192038e8d01006a1
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-06-17 10:20:54 -0300

    Feat: deletar jogo

--- Flask/jogoteca/views.py
+++ Flask/jogoteca/views.py
@@ -17,6 +17,39 @@ def novo():
   
   return render_template('novo.html', titulo='Novo Jogo')
 
+
+@app.route('/editar/<int:id>')
+def editar(id):
+  if 'usuario_logado' not in session or session['usuario_logado'] == None:
+    return redirect(url_for('login', proxima=url_for('editar')))
+  
+  jogo = Jogos.query.filter_by(id=id).first()
+  return render_template('editar.html', titulo='Editando Jogo', jogo=jogo)
+
+@app.route('/atualizar', methods=['POST',])
+def atualizar():
+  jogo = Jogos.query.filter_by(id=request.form['id']).first()
+  jogo.nome = request.form['nome']
+  jogo.categoria = request.form['categoria']
+  jogo.console = request.form['console']
+  
+  db.session.add(jogo)
+  db.session.commit()
+
+  return redirect(url_for('index'))
+
+@app.route('/deletar/<int:id>')
+def deletar(id):
+  if 'usuario_logado' not in session or session['usuario_logado'] == None:
+    return redirect(url_for('login'))
+  
+  Jogos.query.filter_by(id=id).delete()
+  db.session.commit()
+  flash('Jogo Deletado com sucesso!')
+
+  return redirect(url_for('index'))
+
+
 @app.route('/criar', methods=['POST',])
 def criar():
   # Reconhce  campos prenchidos no form
