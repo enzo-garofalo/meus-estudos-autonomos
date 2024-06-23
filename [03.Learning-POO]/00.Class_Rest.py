2a250901aa6872f01333172a369784e3b409a3eb
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-06-22 21:30:22 -0300

    Learning Property and Classmethod

--- [03.Learning-POO]/00.Class_Rest.py
+++ [03.Learning-POO]/00.Class_Rest.py
@@ -1,31 +1,41 @@
 class Restaurante():
   restaurantes = []
-  
   # Self é a referencia atual da instância que está chamando
-  def __init__(self, nome, categoria, ativo, capacidade, nota_avaliacao):
-      self.nome = nome
-      self.categoria = categoria
-      self.ativo = ativo
-      self.capacidade = capacidade
-      self.nota_avaliacao = nota_avaliacao
+  def __init__(self, nome, categoria, capacidade, nota_avaliacao):
+      # Com o '_' o atributo se torna protegido/privado, ou seja, ele não é modificavel pelo user ou por outras classes  
+      self._nome = nome.title()
+      self._categoria = categoria
+      self._capacidade = capacidade
+      self._nota_avaliacao = nota_avaliacao
+      self._ativo = False
 
       Restaurante.restaurantes.append(self)
-  
   # STR é uma maneira de retornar o objeto em formato de texto
   def __str__(self):
-      return f'{self.nome}\n \
-               {self.categoria}\n \
-               Ativo : {self.ativo}\n \
-               Capacidade: {self.capacidade} Pessoas\n \
-               Nota: {self.nota_avaliacao}'
-
-  def lista_restaurantes():
-     for restaurante in Restaurante.restaurantes:
+      return   f'{self._nome}\n' \
+               f'               Categoria: {self._categoria}\n' \
+               f'               Ativo : {self.ativo}\n' \
+               f'               Capacidade: {self._capacidade} Pessoas\n' \
+               f'               Nota: {self._nota_avaliacao}'
+  #Indicando um método da classe   
+  @classmethod
+  def lista_restaurantes(cls):
+     for restaurante in cls.restaurantes:
         print(restaurante)
-        
+  
+  #Muda como o atributo vai ser lido/impresso, poderia fazer cálculos aqui   
+  @property
+  def ativo(self):
+     return 'Ativo ✅' if self._ativo else 'Desativado ❎'
+  
+  def alternar_estado(self):
+     self._ativo = not self._ativo
 
-restaurante1 = Restaurante('Seu Mário', 'Brasileiro', True, 100, '5 Estrelas')
-restaurante1 = Restaurante('Seu Joaquim', 'Italiano', True, 150, '5 Estrelas')
-restaurante1 = Restaurante('Temakeria.com', 'Ruim', True, 300, '2 Estrelas')
+
+restaurante1 = Restaurante('seu Mário', 'Brasileiro', 100, '5 Estrelas')
+restaurante1.alternar_estado()
+
+restaurante2 = Restaurante('Seu Joaquim', 'Italiano',  150, '5 Estrelas')
+restaurante3 = Restaurante('Temakeria.com', 'Ruim',  300, '2 Estrelas')
 
 Restaurante.lista_restaurantes()
