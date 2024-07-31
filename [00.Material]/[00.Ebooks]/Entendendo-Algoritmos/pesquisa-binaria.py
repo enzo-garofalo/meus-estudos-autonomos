ece287a7a221afce1ac09882fdcc37f9d59b3e45
enzo-garofalo <enzo.gp3@puccampinas.edu.br>
2024-07-31 18:42:02 -0300

    New Exercises

---
+++ [00.Material]/[00.Ebooks]/Entendendo-Algoritmos/pesquisa-binaria.py
@@ -0,0 +1,26 @@
+def pesquisa_binaria(lista, item):
+    baixo = 0
+    alto = len(lista) - 1
+    while baixo <= alto:
+        meio = (baixo + alto) // 2
+        chute = lista[meio]
+        if chute == item:
+            return meio
+        if chute > item:
+            alto = meio - 1
+        else:
+            baixo = meio + 1
+    return None
+
+minha_lista = [1, 3, 5, 7, 9]
+print(pesquisa_binaria(minha_lista, 3))
+
+def pesquisa_simples(lista, alvo):
+    for index, num in enumerate(lista, start=0):
+        if num == alvo:
+            return index
+    return None
+
+lista = range(100)
+
+print(pesquisa_simples(lista, 100))
